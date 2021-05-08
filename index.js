dec2aafbc9f2818df84e27b5d70341ed10fbb80c
chenmoonmo <chenmoonmo@gmail.com>
2021-05-08 22:34:31 +0800

    use bodyparder

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 const Koa = require('koa')
 const Router = require('koa-router')
 const route = new Router()
+const bodyParser = require('koa-bodyparser')
 
 const app = new Koa()
+app.use(bodyParser())
 
-console.log(require('./routes/home.js'))
 app.use(async (ctx, next) => {
-  ctx.headers.token = '12345'
   await next()
-  ctx.body += 123
 }).use(require('./routes/home.js').routes())
   .use(route.allowedMethods())
 
--- routes/home.js
+++ routes/home.js
@@ -1,10 +1,15 @@
 const router = require('koa-router')();
 
+
 router.get('/home', async ctx => {
-  console.log(ctx.headers)
   const { name } = ctx.query
   const html = `<h1 style="color: #fe3f10">fuck ${name || 'world'}</h1>`
   ctx.body = html
+})
+
+router.post('/home', async ctx => {
+  console.log(ctx.request.body)
+
 })
 
 module.exports = router
